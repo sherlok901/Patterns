60204f4ebcfda15ac5330fc0c0694dadbe05f316
ibuhaienko <igorbugaenko@gmail.com>
2017-12-27 17:31:18 +0200

    Chain Of Responsibility
    Chain Of Responsibility

---
+++ Patterns/ChainOfResponsibility/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            h1.Successor = h2;
+            h1.HandleRequest(2);
+        }
+    }
+
+    abstract class Handler
+    {
+        public Handler Successor { get; set; }
+        public abstract void HandleRequest(int condition);
+    }
+
+    class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int condition)
+        {
+            if (condition == 1)
+            {
+                // обработка;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(condition);
+            }
+        }
+    }
+
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int condition)
+        {
+            if (condition == 2)
+            {
+                // обработка;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(condition);
+            }
+        }
+    }
+}
---
+++ Patterns/ChainOfResponsibility2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver receiver = new Receiver(false, true, true);
+
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHnadler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+            bankPaymentHandler.Successor = paypalPaymentHandler;
+            paypalPaymentHandler.Successor = moneyPaymentHnadler;
+
+            bankPaymentHandler.Handle(receiver);
+
+            Console.Read();
+        }
+
+        class Receiver
+        {
+            // банковские переводы
+            public bool BankTransfer { get; set; }
+            // денежные переводы - WesternUnion, Unistream
+            public bool MoneyTransfer { get; set; }
+            // перевод через PayPal
+            public bool PayPalTransfer { get; set; }
+            public Receiver(bool bt, bool mt, bool ppt)
+            {
+                BankTransfer = bt;
+                MoneyTransfer = mt;
+                PayPalTransfer = ppt;
+            }
+        }
+        abstract class PaymentHandler
+        {
+            public PaymentHandler Successor { get; set; }
+            public abstract void Handle(Receiver receiver);
+        }
+
+        class BankPaymentHandler : PaymentHandler
+        {
+            public override void Handle(Receiver receiver)
+            {
+                if (receiver.BankTransfer == true)
+                    Console.WriteLine("Executing bank transfer");
+                else if (Successor != null)
+                    Successor.Handle(receiver);
+            }
+        }
+
+        class PayPalPaymentHandler : PaymentHandler
+        {
+            public override void Handle(Receiver receiver)
+            {
+                if (receiver.PayPalTransfer == true)
+                    Console.WriteLine("Executing PayPal transfer");
+                else if (Successor != null)
+                    Successor.Handle(receiver);
+            }
+        }
+        // переводы с помощью системы денежных переводов
+        class MoneyPaymentHandler : PaymentHandler
+        {
+            public override void Handle(Receiver receiver)
+            {
+                if (receiver.MoneyTransfer == true)
+                    Console.WriteLine("Executing money transfer via money system");
+                else if (Successor != null)
+                    Successor.Handle(receiver);
+            }
+        }
+    }
+}
